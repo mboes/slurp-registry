be71f3fe9dc66061f88303c9eeb7124f454fd5bc
Mathieu Boespflug <m@tweag.io>
2018-01-11 22:00:49 +0100

    Revert adding a shell.nix file.
    Not needed for now.

--- shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {}, ghc ? pkgs.ghc }:
-
-pkgs.haskell.lib.buildStackProject {
-  name = "slurp-repository";
-  inherit ghc;
-  buildInputs = with pkgs; [ zlib ];
-  LANG = "en_GB.UTF-8";
-}
